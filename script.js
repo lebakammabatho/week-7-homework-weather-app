4dadeb769e64d94261f41a43afb71f15ce4220f7
Mmabatho <lebakammabatho@gmail.com>
2024-03-12 23:04:47 +0200

    changed the js file and started a new css file

--- script.js
+++ script.js
@@ -1,9 +1,55 @@
-function getWeather() 
-{ const city = document.getElementById('city-input').value; 
-const apiKey = d059510f0t7f54boed6ea43f3f206f9c; 
- const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`; fetch(url) .then(response => response.json()) .then(data => { displayWeather(data); })
- 
- .catch(error => { console.log('Error fetching weather data:', error); }); } 
-function displayWeather(data) { const weatherDetails = document.getElementById('weather-details'); 
-weatherDetails.innerHTML = ` <h3>${data.name}</h3> <p>Temperature: ${data.main.temp}°C</p> <p>Description: ${data.weather[0].description}</p> `; }
 
+function search(event) {
+  event.preventDefault();
+  let searchInputElement = document.querySelector("#city-input");
+  let cityElement = document.querySelector("#current-city");
+  cityElement.innerHTML = searchInputElement.value;
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let day = date.getDay();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let formattedDay = days[day];
+  return `${formattedDay} ${hours}:${minutes}`;
+}
+let city = "Pretoria";
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", search);
+
+let currentDateELement = document.querySelector("#current-date");
+let currentDate = new Date();
+currentDateELement.innerHTML = formatDate(currentDate);
+
+let apiKey = "d059510f0t7f54boed6ea43f3f206f9c";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+let temperature = 7;
+function showWeather(response) {
+  console.log(response.data.temperature.current);
+  let temperatureElement = document.querySelector("#current-temperature-value");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  let cityElement = document.querySelector("#current-city");
+  cityElement.innerHTML = response.data.city;
+  let city = response.data.city;
+}
+
+axios.get(apiUrl).then(showWeather);
