a0644bf900b67d4b3267c238d07022db07e7f0ce
Mmabatho <lebakammabatho@gmail.com>
2024-03-21 17:33:33 +0200

    inserted weather data

--- script.js
+++ script.js
@@ -1,7 +1,39 @@
 function updateWeather(response) {
     let weatherValueElement = document.querySelector("#weather-value");
     let weatherValue = response.data.temperature.current;
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windElement = document.querySelector("#wind");
+    let timeElement = document.querySelector("#time");
+    let date = new Date(response.data.time * 1000);
+
+
+    timeElement.innerHTML = formatDate(date);
+    descriptionElement.innerHTML = response.data.condition.description;
     weatherValueElement.innerHTML = Math.round(weatherValue);
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+    windElement.innerHTML = `${response.data.wind.speed}Km/h`
+
+}
+function formatDate(date) {
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+    let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"
+    ];
+    let day = days[date.getDay()];
+
+    if (minutes < 10) {
+        minutes = '0${minutes}';
+    }
+    
+    return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
